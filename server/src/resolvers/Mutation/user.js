fc5387b84ddeebde26b70804a5f5958e7b2ae972
fonzied <you.dunno@gmail.com>
2018-03-11 19:56:49 -0400

    starting friendslist

--- server/src/resolvers/Mutation/user.js
+++ server/src/resolvers/Mutation/user.js
@@ -3,17 +3,17 @@ const { getUserId } = require('../../utils')
 const user = {
   async addFriend(parent, { target }, ctx, info) {
     const userId = getUserId(ctx)
-    const receiver = await ctx.db.query.user({ where: { id: target } })
-    console.log("RECEEV", receiver)
 
-    if (receiver == null) {
+    if (target == null) {
       return { data: null }
     }
     return ctx.db.mutation.updateUser({
       where: { id: userId },
       data: {
         friends: {
-          name: target
+          set: {
+            id: target
+          }
         }
       }
     })
