295c2b0bd65af1bd8819a2d695ea6be7c491da4b
fonzied <you.dunno@gmail.com>
2018-04-12 15:11:25 -0400

    infinite scroll mini chat

--- src/Routes/FeedPage.js
+++ src/Routes/FeedPage.js
@@ -7,12 +7,38 @@ import { withRouter } from 'react-router-dom'
 import Loading from '../components/Loading'
 
 import 'tachyons'
+import { Visibility, Icon } from 'semantic-ui-react'
 
 class FeedPage extends React.Component {
+  state = {
+    disablePage: false,
+    count: 0
+  }
   componentWillReceiveProps(nextProps) {
     if (this.props.location.key !== nextProps.location.key) {
       this.props.feedQuery.refetch()
     }
+  }
+
+  onFetchMore = () => {
+    const { fetchMore } = this.props.feedQuery;
+    const feedQuery = this.props.feedQuery.allFeed
+    const after = feedQuery[feedQuery.length - 1].id.toString()
+
+    setTimeout(this.setState({ disablePage: true }), 500)
+
+    this.setState({ count: this.state.count + 1 })
+    fetchMore({
+      query: FEED_QUERY,
+      variables: { first: 5, after },
+      updateQuery: (previousResult, { fetchMoreResult, queryVariables }) => {
+        return {
+          allFeed: [
+            ...previousResult.allFeed, ...fetchMoreResult.allFeed,
+          ],
+        };
+      },
+    });
   }
 
   render() {
@@ -25,24 +51,31 @@ class FeedPage extends React.Component {
     }
 
     return (
-      <React.Fragment >
+      <React.Fragment>
         {this.props.feedQuery.allFeed &&
+          !this.props.feedQuery.loading &&
           this.props.feedQuery.allFeed.map(post => (
             <Post
-              key={post.id}
-              post={post}
+              key={!this.props.feedQuery.loading && post.id}
+              post={!this.props.feedQuery.loading && post}
               refresh={() => this.props.feedQuery.refetch()}
             />
           ))}
-        {this.props.children}
+        <Icon style={{ paddingLeft: '50%' }} name='chevron down' />
+        <Visibility
+          continuous={true}
+          onTopVisible={() => this.onFetchMore()}
+        >
+
+        </Visibility>
       </React.Fragment>
     )
   }
 }
 
 const FEED_QUERY = gql`
-  query FeedQuery {
-    allFeed(orderBy: createdAt_DESC){
+  query FeedQuery($first: Int, $after: String, $last: Int, $before: String){
+    allFeed(orderBy: createdAt_DESC, first: $first, last: $last, before: $before, after: $after){
       id
       text
       title
@@ -60,6 +93,12 @@ export default compose(
     name: "feedQuery", // name of the injected prop: this.props.feedQuery...
     options: {
       fetchPolicy: "cache-first",
+      variables: {
+        first: 5,
+        after: undefined,
+        last: undefined,
+        before: undefined
+      },
     },
   }),
-withRouter)(FeedPage)
+  withRouter)(FeedPage)
--- src/components/FriendModal.js
+++ src/components/FriendModal.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import { Button, Modal, Header } from 'semantic-ui-react'
+import { Button, Modal, Header, Icon } from 'semantic-ui-react'
 import gql from 'graphql-tag'
 import moment from 'moment'
 import { Query } from 'react-apollo'
 import { Link } from 'react-router-dom'
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import MessageBar from './MessageBar'
 import client from '../apollo'
 
 import 'tachyons'
 
+var _ = require('lodash');
+
 class FriendModal extends React.Component {
   state = {
     data: '',
+    last: '',
+    after: '',
   }
 
   modalOpen = async () => {
@@ -39,14 +44,46 @@ class FriendModal extends React.Component {
           <Link to={`/profile/${this.props.friend.id}`}>{this.props.friend.name}</Link>
         </Header>
 
-        <Query query={BOX_QUERY} variables={{ sender: this.props.friend.id }}>
-          {({ loading, data, subscribeToMore }) => {
+        <Query query={BOX_QUERY} variables={{ sender: this.props.friend.id, first: 10, before: undefined }}>
+          {({ loading, data, subscribeToMore, fetchMore }) => {
             if (loading) {
               return null;
+            }
+            let before = this.state.last === '' ? data.boxQuery[0].id : this.state.last
+
+            const onFetchMore = (values) => {
+              if (data.boxQuery.length >= 1) {
+
+                fetchMore({
+                  query: BOX_QUERY,
+                  variables: { last: 10, after: before, sender: this.props.friend.id },
+                  updateQuery: (previousResult, { fetchMoreResult, queryVariables }) => {
+
+                    if ([...fetchMoreResult.boxQuery].length >= 1) {
+                      this.setState({ last: fetchMoreResult.boxQuery[0].id })
+                      const response = {
+                        boxQuery: [...previousResult.boxQuery, ...fetchMoreResult.boxQuery]
+                      }
+                      var filteredArray = _.uniqBy(response.boxQuery, function (x) { return x.id }).sort(function (a, b) {
+                        if (a.createdAt > b.createdAt) {
+                          return true
+                        }
+                        return filteredArray
+                      })
+                      if (response.boxQuery.length > 1) {
+                        return {
+                          boxQuery: [...filteredArray]
+                        }
+                      }
+                    }
+                  },
+                });
+              }
             }
 
             subscribeToMore({
               document: MESSAGE_SUBSCRIPTION,
+              variables: { first: 10 },
               updateQuery: (prev, { subscriptionData }) => {
                 if (!subscriptionData.data) return prev;
                 const message = subscriptionData.data.message.node;
@@ -57,24 +94,35 @@ class FriendModal extends React.Component {
               }
             });
             return (
-              <div className="flex flex-column pb3 pt3">
-                {data && data.boxQuery.map(message => {
-                  if (message.sender.id === this.props.friend.id) {
+              <Scrollbars
+                onScrollFrame={(values) => {
+                  if (values.top === 0) onFetchMore(values)
+                }}
+                style={{ width: undefined, height: 200 }}>
+                <div className="flex flex-column pb3 pt3">
+                  <Icon style={{ paddingLeft: '50%' }} name='chevron up' />
+
+
+                  {data && data.boxQuery.map(message => {
+                    if (message.sender.id === this.props.friend.id) {
+                      return (
+                        <div style={{ float: 'left', paddingLeft: '1rem' }} key={message.id}>
+                          {message.text} <span style={{ fontSize: '.7rem' }}>@ {moment(message.createdAt).format('ddd, h:mm a')}</span>
+                        </div>
+                      )
+                    }
                     return (
-                      <div style={{ float: 'left', paddingLeft: '1rem' }} key={message.id}>
+                      <div style={{ float: 'right', marginLeft: 'auto', paddingRight: '1rem' }} key={message.id}>
                         {message.text} <span style={{ fontSize: '.7rem' }}>@ {moment(message.createdAt).format('ddd, h:mm a')}</span>
                       </div>
                     )
-                  }
-                  return (
-                    <div style={{ float: 'right', marginLeft: 'auto', paddingRight: '1rem' }} key={message.id}>
-                      {message.text} <span style={{ fontSize: '.7rem' }}>@ {moment(message.createdAt).format('ddd, h:mm a')}</span>
-                    </div>
-                  )
-                })}
-              </div>
+                  })}
+
+                </div>
+              </Scrollbars>
             )
           }}
+
         </Query>
 
         <MessageBar target={this.props.friend.name} />
@@ -84,8 +132,8 @@ class FriendModal extends React.Component {
 }
 
 const BOX_QUERY = gql`
-query boxQuery($sender: ID!){
-          boxQuery(sender: $sender, orderBy: createdAt_DESC){
+query boxQuery($sender: ID!, $first: Int, $last: Int, $before: String){
+          boxQuery(first: $first, last: $last, before: $before, sender: $sender, orderBy: createdAt_DESC){
           id
       text
         sender {
