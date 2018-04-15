8ffeabd5679602d677936fee8cec22c98aef3a74
fonzied <you.dunno@gmail.com>
2018-04-15 19:33:32 -0400

    deploying

--- src/apollo.js
+++ src/apollo.js
@@ -19,7 +19,6 @@ const middlewareLink = setContext(() => ({
 const afterwareLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
