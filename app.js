7f69667b7f22121e64e9c5c8b816d931395a22c4
Phil <you.dunno@gmail.com>
2018-07-24 15:48:04 -0400

    maybe

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+let app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
